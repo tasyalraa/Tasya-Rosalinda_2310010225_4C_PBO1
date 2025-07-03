542a240657a961c38d7b5f48390df93ceaf1720a
tasyalraa <tasyacaa9@gmail.com>
2025-07-03 20:15:04 +0800

    Menambahkan atribut: tanggal dan jenisLatihan

--- src/manajemenolahraga/Latihan.java
+++ src/manajemenolahraga/Latihan.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package manajemenolahraga;
 
-/**
- *
- * @author tasya
- */
-public class Latihan {
-    
+public class Latihan extends KegiatanOlahraga {
+    private String tanggal;
+    private String jenisLatihan; 
 }
