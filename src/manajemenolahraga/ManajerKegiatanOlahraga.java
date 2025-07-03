33edef1b9e261e2047d78b69adab0cc1ea81910a
tasyalraa <tasyacaa9@gmail.com>
2025-07-03 21:08:07 +0800

    Menggunakan Scanner untuk input data

--- src/manajemenolahraga/ManajerKegiatanOlahraga.java
+++ src/manajemenolahraga/ManajerKegiatanOlahraga.java
@@ -1,7 +1,15 @@
 
 package manajemenolahraga;
 import java.util.ArrayList;
+import java.util.Scanner;
 public class ManajerKegiatanOlahraga {
     private static ArrayList<Anggota> anggotaList = new ArrayList<>();
     private static ArrayList<Latihan> latihanList = new ArrayList<>();
+    
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            
+        }
+    }
+       
 }
