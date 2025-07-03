c547b9b3915102b4b0901ef54996c25ebc17c701
tasyalraa <tasyacaa9@gmail.com>
2025-07-03 20:17:41 +0800

    Membuat constructor untuk inisialisasi data latihan

--- src/manajemenolahraga/Latihan.java
+++ src/manajemenolahraga/Latihan.java
@@ -3,5 +3,12 @@ package manajemenolahraga;
 
 public class Latihan extends KegiatanOlahraga {
     private String tanggal;
-    private String jenisLatihan; 
+    private String jenisLatihan;
+    
+    public Latihan(String tanggal, String jenisLatihan) {
+        super(jenisLatihan);
+        this.tanggal = tanggal;
+        this.jenisLatihan = jenisLatihan;
+    }
+    
 }
