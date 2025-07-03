38ab47a95d3e1cfd8eb638885343c3426e8e96ee
tasyalraa <tasyacaa9@gmail.com>
2025-07-03 20:02:25 +0800

    Menambahkan atribut: nama, usia, olahraga

--- src/manajemenolahraga/Anggota.java
+++ src/manajemenolahraga/Anggota.java
@@ -1,13 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package manajemenolahraga;
 
-/**
- *
- * @author tasya
- */
-public class Anggota {
-    
+
+public class Anggota extends KegiatanOlahraga {
+    private String nama;
+    private int usia;
+    private String olahraga;
 }
