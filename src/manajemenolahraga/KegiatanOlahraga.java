9cd4b8a1b6592623527db32cee50832e3ac9a265
tasyalraa <tasyacaa9@gmail.com>
2025-07-03 19:57:11 +0800

    Menyediakan method accessor getJenisKegiatan()

--- src/manajemenolahraga/KegiatanOlahraga.java
+++ src/manajemenolahraga/KegiatanOlahraga.java
@@ -5,6 +5,11 @@ public class KegiatanOlahraga {
     protected String jenisKegiatan;
     
     public KegiatanOlahraga(String jenisKegiatan) {
+        
         this.jenisKegiatan = jenisKegiatan;
+    }
+    
+    public String getJenisKegiatan() {
+        return jenisKegiatan;
     }
 }
