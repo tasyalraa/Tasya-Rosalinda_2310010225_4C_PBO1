c0ebd03387012a2fe808db7032a0c02c18fa4ab2
tasyalraa <tasyacaa9@gmail.com>
2025-07-03 20:04:34 +0800

    Membuat constructor dengan parameter nama, usia, olahraga

--- src/manajemenolahraga/Anggota.java
+++ src/manajemenolahraga/Anggota.java
@@ -6,4 +6,11 @@ public class Anggota extends KegiatanOlahraga {
     private String nama;
     private int usia;
     private String olahraga;
+    
+    public Anggota(String nama, int usia, String olahraga) {
+        super(olahraga);
+        this.nama = nama;
+        this.usia = usia;
+        this.olahraga = olahraga;
+    }
 }
