1a3819ca7c69ba37aa0072c5baf638a8184b71ac
tasyalraa <tasyacaa9@gmail.com>
2025-07-03 19:53:49 +0800

    Menambahkan constructor untuk inisialisasi jenis kegiatan

--- src/manajemenolahraga/KegiatanOlahraga.java
+++ src/manajemenolahraga/KegiatanOlahraga.java
@@ -3,4 +3,8 @@ package manajemenolahraga;
 
 public class KegiatanOlahraga {
     protected String jenisKegiatan;
+    
+    public KegiatanOlahraga(String jenisKegiatan) {
+        this.jenisKegiatan = jenisKegiatan;
+    }
 }
