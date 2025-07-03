d455b22a2e212ed170f1efd2724d08afbc4bd4e5
tasyalraa <tasyacaa9@gmail.com>
2025-07-03 21:21:34 +0800

    Menambahkan perulangan dengan do-while dan pemilihan menu dengan switch

--- src/manajemenolahraga/ManajerKegiatanOlahraga.java
+++ src/manajemenolahraga/ManajerKegiatanOlahraga.java
@@ -8,7 +8,37 @@ public class ManajerKegiatanOlahraga {
     
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
-            
+            int pilihan;
+            do {
+                System.out.println("=== Aplikasi Manajemen Kegiatan Olahraga ===");
+                System.out.println("1. Pendaftaran Anggota");
+                System.out.println("2. Penjadwalan Latihan");
+                System.out.println("3. Melihat Anggota");
+                System.out.println("4. Melihat Jadwal Latihan");
+                System.out.println("0. Keluar");
+                System.out.print("Pilih menu: ");
+                pilihan = getValidInteger(scanner);
+                
+                switch (pilihan) {
+                    case 1:
+                        daftarAnggota(scanner);
+                        break;
+                    case 2:
+                        jadwalkanLatihan(scanner);
+                        break;
+                    case 3:
+                        lihatAnggota();
+                        break;
+                    case 4:
+                        lihatJadwalLatihan();
+                        break;
+                    case 0:
+                        System.out.println("Keluar dari aplikasi.");
+                        break;
+                    default:
+                        System.out.println("Pilihan tidak valid. Silakan coba lagi.");
+                }
+            } while (pilihan != 0);
         }
     }
        
